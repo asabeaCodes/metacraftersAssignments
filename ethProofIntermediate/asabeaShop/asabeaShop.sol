41d3d9cc48d8a48b051cfe5832c10c3925f0c172
asabeaCodes <142179240+asabeaCodes@users.noreply.github.com>
2023-10-14 20:54:18 +0000

    Update asabeaShop.sol

--- ethProofIntermediate/asabeaShop/asabeaShop.sol
+++ ethProofIntermediate/asabeaShop/asabeaShop.sol
@@ -1,1 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract asabeaShop {
+
+    mapping (address => possessions) users;
+
+    struct possessions {
+        uint bags;
+        uint shoes;
+    }
+    
+    function buyBag() payable public {
+        require(msg.value >= 2 ether, "The bag costs more than the amount you paid");
+        users[msg.sender].bags +=1;
+    }
+
+    function buyShoe() payable public {
+        if (msg.value <= 4 ether) {
+            revert("you can't get a shoe this cheap");
+        }
+        users[msg.sender].shoes += 1;
+    }
+
+    function checkPossessions() public view returns (possessions memory) {
+        return users[msg.sender];
+    }
+
+
+    address payable public shopOwner;
+
+    constructor() {
+        shopOwner = payable(msg.sender);
+    }
+
+    function withdrawSales() public returns (bool sent) {
+        assert(msg.sender == shopOwner);
+        
+        sent = shopOwner.send(address(this).balance);
+    }
+}
 
